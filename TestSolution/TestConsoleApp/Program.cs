396531bd700f0a6f8dc8a0bd96aba89fbc79c273
htjurkmen <hjusein.tjurkmen@gmail.com>
2014-08-25 17:52:21 +0300

    Created New Solution

---
+++ TestSolution/TestConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TestLibrary;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestClass a = new TestClass();
+            Console.WriteLine(a.CurrentDate());
+        }
+    }
+}
---
+++ TestSolution/TestLibrary/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public string CurrentDate()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
